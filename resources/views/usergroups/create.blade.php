44181734e189362fce76cc60d930cd5aed308c83
NOH <mahmoudnoh@awstreams.com>
2017-11-16 19:31:06 +0200

    User Groups - finish

--- resources/views/usergroups/create.blade.php
+++ resources/views/usergroups/create.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <div class="row">
     <div class="col-lg-12 ks-panels-column-section">
         <div class="card">
@@ -5,6 +8,14 @@
                 <h5 class="card-title">Create new User Group</h5>
 
                 {!! Form::open(['route' => 'usergroups.store']) !!}
+
+                @if (isset($repeat))
+                    <div class="alert alert-danger">
+                        <ul>
+                            <li class="text-danger"><b>{{ $repeat }}</b></li>
+                        </ul>
+                    </div>
+                @endif
 
                 @include('usergroups.fields')
 
@@ -14,3 +25,4 @@
         </div>
     </div>
 </div>
+@endsection

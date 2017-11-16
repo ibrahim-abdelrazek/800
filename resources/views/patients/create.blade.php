e5d4956632eca121a582c4c58cab98e56dd8ceed
Ahmed Atef <ahmed.atef@awstreams.com>
2017-11-16 22:04:03 +0200

    updating patients

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -1,75 +1,17 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="row">
-        <div class="col-lg-12 ks-panels-column-section">
-            <div class="card">
-                <div class="card-block">
+<div class="row">
+    <div class="col-lg-12 ks-panels-column-section">
+        <div class="card">
+            <div class="card-block">
                     <h5 class="card-title">Create new Patient</h5>
 
                         {!! Form::open(['route' => 'patients.store']) !!}
 
                             @include('patients.fields')
 
-                        {!! Form::close() !!}
-                    </div>
-                </div>
+                {!! Form::close() !!}
 
             </div>
         </div>
-@endsection
-@push('customjs')
-    <script type="text/javascript">
-        $(document).ready(function() {
 
-            $(".office").on("click", function () {
-
-                $('#office').css("display", 'block');
-                $('#home').css("display", 'none');
-
-                $('#apartment').css("display", 'none');
-                $('#villa').css("display", 'none');
-
-            /*    $(".apartmentName").val('');
-               $(".apartmentNumber").val('');
-                $(".villaNumber").val('');*/
-
-            });
-
-            $(".home").on("click", function () {
-
-                $('#home').css("display", 'block');
-                $('#office').css("display", 'none');
-
-
-                $(".villa").on("click", function () {
-
-                    $('#villa').css("display", 'block');
-                    $('#apartment').css("display", 'none');
-/*
-                    $(".compName").val('');
-                    $(".buildName").val('');
-                    $(".officeNumber").val('');
-                    $(".apartmentName").val('');
-                    $(".apartmentNumber").val('');*/
-                });
-
-
-                $(".apartment").on("click", function () {
-
-                    $('#apartment').css("display", 'block');
-                    $('#villa').css("display", 'none');
-
-                   /* $(".compName").val('');
-                    $(".buildName").val('');
-                    $(".officeNumber").val('');
-                    $(".villaNumber").val('');*/
-
-                });
-            });
-
-        });
-
-
-    </script>
-@endpush
+    </div>
+</div>

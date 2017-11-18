f998bab7e66f9ec6edbaf02f25f44905115d25b2
Ahmed Atef <ahmed.atef@awstreams.com>
2017-11-18 14:51:16 +0200

    cleaning repo

---
+++ app/Messenger/MessengerServiceProvider.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Messenger;
+
+use App\Messenger\Models\Message;
+use App\Messenger\Models\Models;
+use App\Messenger\Models\Participant;
+use App\Messenger\Models\Thread;
+use Illuminate\Support\ServiceProvider;
+
+class MessengerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->offerPublishing();
+        $this->setMessengerModels();
+        $this->setUserModel();
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->configure();
+    }
+
+    /**
+     * Setup the configuration for Messenger.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(
+            base_path('app/Messenger/config/config.php'),
+            'messenger'
+        );
+    }
+
+    /**
+     * Setup the resource publishing groups for Messenger.
+     *
+     * @return void
+     */
+    protected function offerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                base_path('app/Messenger/config/config.php') => config_path('messenger.php'),
+            ], 'config');
+
+            $this->publishes([
+                base_path('app/Messenger/migrations') => base_path('database/migrations'),
+            ], 'migrations');
+        }
+    }
+
+    protected function setMessengerModels()
+    {
+        $config = $this->app->make('config');
+
+        Models::setMessageModel($config->get('messenger.message_model', Message::class));
+        Models::setThreadModel($config->get('messenger.thread_model', Thread::class));
+        Models::setParticipantModel($config->get('messenger.participant_model', Participant::class));
+
+        Models::setTables([
+            'messages' => $config->get('messenger.messages_table', Models::message()->getTable()),
+            'participants' => $config->get('messenger.participants_table', Models::participant()->getTable()),
+            'threads' => $config->get('messenger.threads_table', Models::thread()->getTable()),
+        ]);
+    }
+
+    protected function setUserModel()
+    {
+        $config = $this->app->make('config');
+
+        $model = $config->get('auth.providers.users.model', function () use ($config) {
+            return $config->get('auth.model', $config->get('messenger.user_model'));
+        });
+
+        Models::setUserModel($model);
+
+        Models::setTables([
+            'users' => (new $model)->getTable(),
+        ]);
+    }
+}
---
+++ app/Messenger/Traits/Messagable.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Messenger\Traits;
+
+use App\Messenger\Models\Message;
+use App\Messenger\Models\Models;
+use App\Messenger\Models\Participant;
+use App\Messenger\Models\Thread;
+use Illuminate\Database\Eloquent\Builder;
+
+trait Messagable
+{
+    /**
+     * Message relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     *
+     * @codeCoverageIgnore
+     */
+    public function messages()
+    {
+        return $this->hasMany(Models::classname(Message::class));
+    }
+
+    /**
+     * Participants relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     *
+     * @codeCoverageIgnore
+     */
+    public function participants()
+    {
+        return $this->hasMany(Models::classname(Participant::class));
+    }
+
+    /**
+     * Thread relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     *
+     * @codeCoverageIgnore
+     */
+    public function threads()
+    {
+        return $this->belongsToMany(
+            Models::classname(Thread::class),
+            Models::table('participants'),
+            'user_id',
+            'thread_id'
+        );
+    }
+
+    /**
+     * Returns the new messages count for user.
+     *
+     * @return int
+     */
+    public function newThreadsCount()
+    {
+        return $this->threadsWithNewMessages()->count();
+    }
+
+    /**
+     * Returns the new messages count for user.
+     *
+     * @return int
+     */
+    public function unreadMessagesCount()
+    {
+        return Message::unreadForUser($this->getKey())->count();
+    }
+
+    /**
+     * Returns all threads with new messages.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function threadsWithNewMessages()
+    {
+        return $this->threads()
+            ->where(function (Builder $q) {
+                $q->whereNull(Models::table('participants') . '.last_read');
+                $q->orWhere(Models::table('threads') . '.updated_at', '>', $this->getConnection()->raw($this->getConnection()->getTablePrefix() . Models::table('participants') . '.last_read'));
+            })->get();
+    }
+}

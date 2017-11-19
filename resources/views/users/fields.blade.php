6f0f4b50f8685f4ff9f46f7f90d66675d25a9ef6
noh <mahmoudnoh@awstreams.com>
2017-11-19 20:37:33 +0200

    User

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -14,7 +14,7 @@
 <div class="form-group row">
     <label for="default-input" class="col-sm-2 form-control-label">{!! Form::label('name', 'Name:') !!}</label>
     <div class="col-sm-10">
-        {!! Form::text('name', null, [  'placeholder'=>'Enter Partner\'s name', 'class' => 'form-control']) !!}
+        {!! Form::text('name', null, [   'class' => 'form-control']) !!}
     </div>
 </div>
 <!--  username -->
@@ -31,6 +31,16 @@
         {!! Form::email('email', null, [  'class' => 'form-control']) !!}
     </div>
 </div>
+<!--  image -->
+<div class="form-group row">
+    <label for="default-input" class="col-sm-2 form-control-label">
+        {!! Form::label('avatar', 'Image:') !!}</label>
+    <div class="col-sm-10">
+        {!! Form::file('avatar', null, [  'class' => 'form-control']) !!}
+    </div>
+</div>
+
+
 <!--  password -->
 <div class="form-group row">
     <label for="default-input" class="col-sm-2 form-control-label">{!! Form::label('password', 'Password:') !!}</label>
@@ -38,34 +48,52 @@
         {!! Form::password('password',[  'class' => 'form-control']) !!}
         <br>
         <b class="text-warning"> Your Password must contain at least 6 characters as (Uppercase and Lowercase characters and Numbers and Special characters). </b>
-
     </div>
 </div>
-
-<!--  Status -->
 <div class="form-group row">
-    <label for="default-input"
-           class="col-sm-2 form-control-label">{!! Form::label('user_group_id', 'User Group:') !!}</label>
+    <label for="default-input" class="col-sm-2 form-control-label">{!! Form::label('password_confirmation', 'Confirm Password:') !!}</label>
     <div class="col-sm-10">
-        @if(Auth::user()->isAdmin())
-            {!! Form::select('user_group_id', \App\UserGroup::where('id','!=',1)->where('id','!=',2)->pluck('group_name','id'), null, [  'class' => 'form-control']) !!}
-        @else
-            {!! Form::select('user_group_id', \App\UserGroup::where('partner_id',Auth::user()->partner_id)->pluck('group_name','id'), null, [  'class' => 'form-control']) !!}
-
-        @endif
+        {!! Form::password('password_confirmation', ['class' => 'form-control'] ) !!}
     </div>
 </div>
+
 @if(Auth::user()->isAdmin())
     <!--  Status -->
     <div class="form-group row">
         <label for="default-input"
                class="col-sm-2 form-control-label">{!! Form::label('partner_id', 'Partner:') !!}</label>
         <div class="col-sm-10">
-            {!! Form::select('partner_id', \App\Partner::all()->pluck('name','id'), null, [  'class' => 'form-control']) !!}
-
+            @if(\App\Partner::count() > 0)
+                {!! Form::select('partner_id',App\Partner::pluck('name','id'),null,['style'=>'width:100% !importnat','class' => 'form-control'])!!}
+            @else
+                <p>You don't have added partners yet, Please <a href="{{route('partners.index')}}"><b
+                                class="label-danger">Add
+                            new Partner</b></a></p>
+            @endif
         </div>
     </div>
 @endif
+
+<!--  user_group_id -->
+<div class="form-group row">
+    <label for="default-input"
+           class="col-sm-2 form-control-label">{!! Form::label('user_group_id', 'User Group:') !!}</label>
+    <div id="usergroups-holder" class="col-sm-10">
+        @if(!isset($user))
+            @if(\App\UserGroup::where('partner_id', Auth::user()->id)->count() > 0)
+                {!! Form::select('user_group_id',App\UserGroup::where('partner_id', Auth::user()->id)->pluck('group_name','id'),null,['class' => 'form-control'])!!}
+            @else
+                <p>You don't have added user groups yet, Please <a href="{{route('usergroups.index')}}"><b class="label-danger">Add
+                            new User Group</b></a></p>
+            @endif
+        @else
+            {!! Form::select('user_group_id',App\UserGroup::pluck('group_name','id'),null,['class' => 'form-control'])!!}
+
+        @endif
+
+    </div>
+</div>
+
 <!-- Submit Field -->
 <div class="form-group col-sm-8 col-sm-offset-2" id='submit'>
 
